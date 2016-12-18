8ed97775e6752b7ca337b036d72dd199ab165bd4
Mark Bann <markbann44@gmail.com>
2016-12-18 13:24:50 -0500

    Removed the Floating Action Button for email. Extracted classes.

---
+++ app/src/main/java/com/example/mark/pantryapp/SectionsPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.mark.pantryapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 4 total pages.
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Items On Hand";
+            case 1:
+                return "Recipes";
+            case 2:
+                return "Shopping List";
+            case 3:
+                return "Coupons";
+        }
+        return null;
+    }
+}
